4400f0cab2969994e35d2a060a3389282ba5128f
ridhima <ridhimavijay2003@gmail.com>
2023-09-30 15:42:00 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -8,3 +8,9 @@ bill_per_person = total_bill / people
 final_amount = bill_per_person
 print(f"if the bill was rs {bill},split b/w {people} with {tip} % tip:")
 print(f"each person should pay rs {final_amount:2f}")
+
+# bill_with_tip=tip/100*bill+bill
+# print(bill_with_tip)
+# bill_per_person=bill_with_tip/people
+# final_amount=round(bill_per_person,2)
+# print(f"Each person should pay: {final_amount}")
